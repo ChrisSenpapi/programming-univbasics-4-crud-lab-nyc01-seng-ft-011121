98d58d19bd27e7b6ea6bca60066f0fdcce838be3
Christopher Read <christopherread22@outlook.com>
2020-12-05 20:22:13 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,7 @@ end
 
 def remove_element_from_end_of_array(array)
   words = ["boom", "bam", "bop", "arrays!"]
-  arrays! = words.pop
+  "arrays!" = words.pop
 end
 
 def remove_element_from_start_of_array(array)
