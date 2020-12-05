31a2d7092bc6057c03fd6bffa2545eeb2507b51b
Christopher Read <christopherread22@outlook.com>
2020-12-05 20:46:07 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -32,8 +32,8 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  woah = ["woop", "wow"]
-  wow[1]
+  woah = ["wow", "woop"]
+  wow[0]
 end
 
 def retrieve_last_element_from_array(array)
