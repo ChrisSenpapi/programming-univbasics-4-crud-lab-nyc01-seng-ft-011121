60f5754de213d4f97ebac83f2c8253824b700331
Christopher Read <christopherread22@outlook.com>
2020-12-05 20:09:27 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  food = ["Pizza", ]
+  food = ["Pizza", "Turkey"]
+  food << 
 end
 
 def add_element_to_start_of_array(array, element)
