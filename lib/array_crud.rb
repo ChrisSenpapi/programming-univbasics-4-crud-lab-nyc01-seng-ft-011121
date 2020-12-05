19f14235b3dae62778b4e7f714c2a7517d59f7de
Christopher Read <christopherread22@outlook.com>
2020-12-05 20:38:43 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,7 +22,8 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  cats = ["Brody", "Sammy", "Bam"]
+  cats = ["wow", "Sammy", "Bam"]
+  wow = cats.shift
 end
 
 def retrieve_element_from_index(array, index_number)
